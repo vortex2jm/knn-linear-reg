673922d718e3ec5ab69c8ff5a3b041eea55516cc
vortex2jm <joaopaulomoura5328@gmail.com>
2023-07-02 21:17:01 -0300

    :trophy: Finishing linear reg and modularizing

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_absolute_error
+from sklearn.linear_model import LinearRegression
+
+from modules.knn import knn_regressor
+from modules.lreg import linear_regressor
 
 def main():
-    import numpy as np
-    import pandas as pd
-
-    from sklearn.neighbors import KNeighborsRegressor
-    from sklearn.model_selection import train_test_split
-    from sklearn.preprocessing import StandardScaler
-    from sklearn.metrics import mean_absolute_error
-
-    #Leitura dos dados
+    # Data reading
     df = pd.read_csv("./data/Automobile.csv")
 
-    #Tratamento de dados
+    # Data processing
     df = df.drop(columns=["name", "origin"]).dropna()
     df['cylinders'] = df['cylinders'].astype(np.float64)
     df['weight'] = df['weight'].astype(np.float64)
@@ -20,39 +23,22 @@ def main():
     inputs = df.iloc[:, 1:]
     output = df.mpg
 
-    final_mae = []
+    # Mean absolute errors
+    final_mae_knn = []
+    final_mae_lr = []
 
     for i in range(30):
-
-        k_mae = []
-        for k in range(10):
-            x_train, x_test, y_train, y_test = train_test_split(inputs, output, test_size=0.1, train_size=0.9)
-
-            scaler = StandardScaler()
-
-            x_train = scaler.fit_transform(x_train)
-            x_test = scaler.fit_transform(x_test)
-
-            knn_regressor = KNeighborsRegressor(n_neighbors=k+1)
-
-            knn_regressor.fit(x_train, y_train)
-
-            y_pred = knn_regressor.predict(x_test)
-
-            mae = mean_absolute_error(y_test, y_pred)
-
-            k_mae.append(mae)
-
+        # KNN regressor ==================================== #
+        k_mae = knn_regressor(inputs, output)
         winner_mae = k_mae[k_mae.index(min(k_mae))]
-        final_mae.append(winner_mae)
+        final_mae_knn.append(winner_mae)
+        
+        #Linear regressor ================================== #
+        linear_mae = linear_regressor(inputs, output)
+        final_mae_lr.append(linear_mae)
 
-    print("valores finais de MAE: \n", final_mae)
-
-    print("MAE médio: ", sum(final_mae)/len(final_mae))
-
-
-
-
+    print("Avarage MAE [KNN]: ", sum(final_mae_knn)/len(final_mae_knn))
+    print('Avarage MAE [LR]: ', sum(final_mae_lr)/len(final_mae_lr))
     
 if __name__ == "__main__":
    main()
