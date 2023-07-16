5aad7eccb9d5ecd9977d93848cf619393d7d98ec
Diogo Delazare <88203023+JoemanJ@users.noreply.github.com>
2023-07-15 23:19:05 -0300

    Added MAE for average value

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from sklearn.linear_model import LinearRegression
 
 from modules.knn import knn_regressor
 from modules.lreg import linear_regressor
+from modules.intervalo_confianca import intervalo_de_confianca
 
 def main():
     # Data reading
@@ -37,12 +38,17 @@ def main():
         linear_mae = linear_regressor(inputs, output)
         final_mae_lr.append(linear_mae)
 
+    print()
     print("Avarage MAE [KNN]: ", sum(final_mae_knn)/len(final_mae_knn))
     print('Avarage MAE [LR]: ', sum(final_mae_lr)/len(final_mae_lr))
-
+    print()
+    print("Intervalo de confiança MAE [KNN, 95%]: ", intervalo_de_confianca(final_mae_knn))
+    print('Intervalo de confiança MAE [LR, 95%]: ', intervalo_de_confianca(final_mae_lr))
+    print()
     average = sum(output)/len(output)
     print("Average value: ", average)
     print("MAE for average value: ", mean_absolute_error(output, len(output)*[average]))
+    print()
     
 if __name__ == "__main__":
    main()
